b371ab0bd00916335a35a24c16df96bf4ab78a52
Tsuibin <xubin@linuxdeepin.com>
2015-09-29 14:49:20 +0800

    update

--- fake-deb.sh
+++ fake-deb.sh
@@ -2,14 +2,14 @@
 #fake-deb <pkg name> <version> 
 
 mkdir  $1
-cd $1 && mkdir -p "$1_$2_sw_64"/DEBIAN
-cp -a ../example_sw_64/DEBIAN/  "$1_$2_sw_64"/
+cd $1 && mkdir -p "$1_$2_alpha"/DEBIAN
+cp -a ../example_alpha/DEBIAN/  "$1_$2_alpha"/
  
 pac=$1
 ver=$2
-sed -i "s/Package:.*/Package: $pac/" "$1_$2_sw_64"/DEBIAN/control
-sed -i "s/Source:.*/Source: $pac/" "$1_$2_sw_64"/DEBIAN/control
-sed -i "s/Version:.*/Version: $ver/" "$1_$2_sw_64"/DEBIAN/control
+sed -i "s/Package:.*/Package: $pac/" "$1_$2_alpha"/DEBIAN/control
+sed -i "s/Source:.*/Source: $pac/" "$1_$2_alpha"/DEBIAN/control
+sed -i "s/Version:.*/Version: $ver/" "$1_$2_alpha"/DEBIAN/control
 
-dpkg-deb -b "$1_$2_sw_64"
-dpkg -i "$1_$2_sw_64.deb"
+dpkg-deb -b "$1_$2_alpha"
+#dpkg -i "$1_$2_alpha.deb"
